e6eb42568166e5bfc1ea8d67435944d8aa22e1e3
Eric Masi <eric.masi90@gmail.com>
2021-08-02 14:09:11 -0400

    Style content

--- front-end/src/features/orders/OpenOrders.js
+++ front-end/src/features/orders/OpenOrders.js
@@ -1,22 +1,62 @@
 import React from 'react';
 import { useGetOrdersQuery } from '../../app/api';
 
-import { Stack } from '@material-ui/core';
+import {
+  Stack,
+  CircularProgress,
+  Skeleton,
+  Alert,
+  AlertTitle,
+  Paper,
+  Container,
+} from '@material-ui/core';
 
 export default function OpenOrders() {
   const { data: orders, error, isLoading } = useGetOrdersQuery();
 
   console.log(orders);
 
-  return isLoading ? (
-    <h1>loading</h1>
-  ) : error ? (
-    <h1>you done messed up a-a-ron!</h1>
-  ) : orders ? (
-    <Stack spacing={2} alignItems="center">
-      {orders.map((o) => (
-        <p key={o.sk}>{o.status}</p>
-      ))}
-    </Stack>
-  ) : null;
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '1rem',
+      }}
+    >
+      {isLoading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          Everything is on fire!
+        </Alert>
+      ) : orders ? (
+        <Stack spacing={2} alignItems="center">
+          {orders.map((o) => (
+            <Order key={o.sk} order={o} />
+          ))}
+        </Stack>
+      ) : (
+        <Skeleton variant="rectangular" width={210} height={118} />
+      )}
+    </Container>
+  );
+}
+
+function Order(props) {
+  const { order } = props;
+
+  return (
+    <Paper elevation={2} sx={{ padding: '.5rem' }}>
+      <Stack spacing={1}>
+        <h3>{`${order.firstName} ${order.lastName}`}</h3>
+        <p>Order Status: {order.status}</p>
+        <p>Total: {order.total}</p>
+        <p>Created: {order.created.split('T')[0]}</p>
+      </Stack>
+    </Paper>
+  );
 }
