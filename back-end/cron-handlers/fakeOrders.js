51b48b3977ec7f9bb2a9978072109b90a4009e2a
Eric Masi <eric.masi90@gmail.com>
2021-08-02 14:54:18 -0400

    Add function to create fake products to test polling

---
+++ back-end/cron-handlers/fakeOrders.js
@@ -0,0 +1,41 @@
+import { name, commerce } from 'faker';
+import { randomInt } from 'd3-random';
+import { createOrder } from '../db/orders';
+
+export const handler = fakeOrders;
+
+async function fakeOrders() {
+  try {
+    const order = {
+      firstName: name.firstName(),
+      lastName: name.lastName(),
+      items: createItems(),
+    };
+    order.total = order.items.reduce((acc, item) => acc + item.price, 0);
+
+    await createOrder(order);
+  } catch (error) {
+    console.info(error);
+  }
+}
+
+function createItems() {
+  const maxFive = randomInt(5);
+  const numItems = maxFive();
+  const items = [];
+
+  for (let i = 0; i < numItems; i++) {
+    const quantity = maxFive();
+    const unitPrice = randomInt(549, 99999)();
+    const item = {
+      name: commerce.productName(),
+      description: commerce.productDescription(),
+      quantity,
+      price: quantity * unitPrice,
+    };
+
+    items.push(item);
+  }
+
+  return items;
+}
