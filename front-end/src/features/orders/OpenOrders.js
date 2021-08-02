897f0dda1eb9af5448619a64868f4611e6f7ccc0
Eric Masi <eric.masi90@gmail.com>
2021-08-02 13:22:35 -0400

    Add material ui

--- front-end/src/features/orders/OpenOrders.js
+++ front-end/src/features/orders/OpenOrders.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { useGetOrdersQuery } from '../../app/api';
 
-export default function OpenOrders() {
-  const { data, error, isLoading } = useGetOrdersQuery();
+import { Stack } from '@material-ui/core';
 
-  console.log(data);
+export default function OpenOrders() {
+  const { data: orders, error, isLoading } = useGetOrdersQuery();
+
+  console.log(orders);
 
   return isLoading ? (
     <h1>loading</h1>
   ) : error ? (
     <h1>you done messed up a-a-ron!</h1>
-  ) : data ? (
-    <h1>got the data!</h1>
+  ) : orders ? (
+    <Stack spacing={2} alignItems="center">
+      {orders.map((o) => (
+        <p key={o.sk}>{o.status}</p>
+      ))}
+    </Stack>
   ) : null;
 }
