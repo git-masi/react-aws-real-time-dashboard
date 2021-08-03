07cc38c13a6e07691cdc5ad10b09916701e32eab
Eric Masi <eric.masi90@gmail.com>
2021-08-02 21:07:55 -0400

    Refactor

--- back-end/web-socket-api/web-socket-handlers/connect.js
+++ back-end/web-socket-api/web-socket-handlers/connect.js
@@ -3,6 +3,20 @@ import { addConnection } from '../db/connections';
 export const handler = connect;
 
 async function connect(event) {
+  const connectionItem = createConnectionItem(event);
+
+  try {
+    await addConnection(connectionItem);
+
+    console.info(
+      `New web socket connection ${connectionId} from origin ${origin} added`
+    );
+  } catch (error) {
+    console.info(error);
+  }
+}
+
+function createConnectionItem(event) {
   const {
     headers: { Origin: origin },
     requestContext: { connectionId, connectedAt, domainName },
@@ -15,13 +29,5 @@ async function connect(event) {
     requestDomain: domainName,
   };
 
-  try {
-    await addConnection(item);
-
-    console.info(
-      `New web socket connection ${connectionId} from origin ${origin} added`
-    );
-  } catch (error) {
-    console.info(error);
-  }
+  return item;
 }
