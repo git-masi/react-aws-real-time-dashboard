c468885956c300de1f8c6957f111a88839becfd5
Eric Masi <eric.masi90@gmail.com>
2021-08-04 10:17:48 -0400

    Add auth token to request

--- front-end/src/app/api.js
+++ front-end/src/app/api.js
@@ -4,6 +4,17 @@ export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://ewpm8v8dp4.execute-api.us-east-1.amazonaws.com/dev/',
+    prepareHeaders: (headers, { getState }) => {
+      // By default, if we have a token in the store, let's use that for authenticated requests
+      // const token = (getState()).auth.token
+      const token = '98765';
+
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
     getOrders: builder.query({
