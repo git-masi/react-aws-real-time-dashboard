9eba9f1a1f876d44f083bed698fbb70b55a5ff5e
Eric Masi <eric.masi90@gmail.com>
2021-08-04 12:50:21 -0400

    Open ws connection

--- front-end/src/app/api.js
+++ front-end/src/app/api.js
@@ -19,6 +19,43 @@ export const api = createApi({
   endpoints: (builder) => ({
     getOrders: builder.query({
       query: () => 'orders',
+      async onCacheEntryAdded(
+        arg,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+      ) {
+        // create a websocket connection when the cache subscription starts
+        let ws = new WebSocket(
+          'wss://4kh1b6ad2e.execute-api.us-east-1.amazonaws.com/dev?authorization=98765'
+        );
+
+        try {
+          // wait for the initial query to resolve before proceeding
+          await cacheDataLoaded;
+
+          // when data is received from the socket connection to the server,
+          // if it is a message and for the appropriate channel,
+          // update our query result with the received message
+          const listener = (event) => {
+            const data = JSON.parse(event.data);
+            // if (!isMessage(data) || data.channel !== arg) return
+
+            updateCachedData((draft) => {
+              draft.push(data);
+            });
+          };
+
+          ws.addEventListener('message', listener);
+        } catch (error) {
+          console.log(error);
+          console.info('Error with WebSocket connection');
+          // no-op in case `cacheEntryRemoved` resolves before `cacheDataLoaded`,
+          // in which case `cacheDataLoaded` will throw
+        }
+        // cacheEntryRemoved will resolve when the cache subscription is no longer active
+        await cacheEntryRemoved;
+        // perform cleanup steps once the `cacheEntryRemoved` promise resolves
+        ws.close?.();
+      },
     }),
     createOrder: builder.mutation({
       query: (body) => ({
