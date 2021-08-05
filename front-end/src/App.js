b801f316c5d74aade427551ea2816d38267a3942
Eric Masi <eric.masi90@gmail.com>
2021-08-05 08:20:25 -0400

    Add component to handle changing views

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Container, Stack } from '@material-ui/core';
 import AllOrders from './features/orders/AllOrders';
 
+const views = Object.freeze({
+  none: 'none',
+  allOrders: 'allOrders',
+});
+
 export default function App() {
-  return <AllOrders />;
+  const [display, setDisplay] = useState(views.none);
+
+  return (
+    <Container maxWidth="md">
+      <Nav setView={setDisplay} />
+      {display === views.none && <h1>Nothing to see here</h1>}
+      {display === views.allOrders && <AllOrders />}
+    </Container>
+  );
+}
+
+function Nav(props) {
+  const { setView } = props;
+  const buttons = Object.values(views).map((path) => (
+    <Button key={path} variant="outlined" onClick={() => setView(path)}>
+      {titleCase(path)}
+    </Button>
+  ));
+
+  return (
+    <Stack direction={'row'} sx={{ padding: '1rem 0' }} spacing={2}>
+      {buttons}
+    </Stack>
+  );
+}
+
+function titleCase(str) {
+  const split = str.replace(/([A-Z]+)*([A-Z][a-z])/g, '$1 $2');
+  return split.substring(0, 1).toUpperCase() + split.substring(1);
 }
