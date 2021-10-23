20277c547055ba3addee7ea478eef4fe770488fc
Eric Masi <eric.masi90@gmail.com>
2021-10-23 13:31:53 -0400

    Enable rule for lambda cron job

--- back-end/http-api/api-handlers/clients.js
+++ back-end/http-api/api-handlers/clients.js
@@ -1,10 +1,19 @@
+import {
+  eventBridge,
+  eventBridgeRuleOperations,
+} from '../../utils/eventBridge';
 import { apiResponse } from '../../utils/http';
 import { commonMiddleware } from '../../utils/middleware';
 
 export const handler = commonMiddleware(clients);
 
-function clients(event) {
+async function clients(event) {
   try {
+    const params = {
+      Name: '', // required
+      EventBusName: '',
+    };
+    await eventBridge.rule(eventBridgeRuleOperations.enable, params);
     return apiResponse({ statusCode: 200, cors: true });
   } catch (error) {
     console.info(error);
