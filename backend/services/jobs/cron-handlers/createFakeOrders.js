333dbf76925abe39e17164ef979c7f5a3a005c45
Eric Masi <eric.masi90@gmail.com>
2021-11-02 20:20:27 -0400

    Fix bugs

--- backend/services/jobs/cron-handlers/createFakeOrders.js
+++ backend/services/jobs/cron-handlers/createFakeOrders.js
@@ -1,44 +1,61 @@
 import { name, commerce } from 'faker';
 import { randomInt } from 'd3-random';
-import { addOrder } from '../../http/db/orders';
+import { createWriteTransactionParams, dynamoDb } from '../../../utils/dynamo';
+import { orderStatuses, pkValues } from '../../../utils/constants';
 
-const TEST_STORE_ID = '98765';
+const { MAIN_TABLE_NAME } = process.env;
 
 export const handler = createFakeOrders;
 
 async function createFakeOrders() {
   try {
-    const order = {
-      firstName: name.firstName(),
-      lastName: name.lastName(),
-      items: createItems(),
-      storeId: TEST_STORE_ID,
-    };
-    order.total = order.items.reduce((acc, item) => acc + item.price, 0);
+    const params = createWriteTransactionParams([
+      MAIN_TABLE_NAME,
+      buildNewOrder('SOME_CLIENT_ID_HERE'), // todo: get all clients and add an order per client
+    ]);
 
-    await addOrder(order);
+    await dynamoDb.transactWrite(params);
   } catch (error) {
     console.info(error);
   }
-}
 
-function createItems() {
-  const maxFive = randomInt(1, 5);
-  const numItems = maxFive();
-  const items = [];
-
-  for (let i = 0; i < numItems; i++) {
-    const quantity = maxFive();
-    const unitPrice = randomInt(549, 99999)();
-    const item = {
-      name: commerce.productName(),
-      description: commerce.productDescription(),
-      quantity,
-      price: quantity * unitPrice,
+  function buildNewOrder(clientId) {
+    const items = createItems();
+    const created = new Date().toISOString();
+    const sk = `${created}#${clientId}`;
+    const order = {
+      firstName: name.firstName(),
+      lastName: name.lastName(),
+      items,
+      clientId,
+      total: items.reduce((acc, item) => acc + item.price, 0),
+      pk: pkValues.order,
+      sk,
+      created,
+      status: orderStatuses.open,
     };
 
-    items.push(item);
-  }
+    return order;
 
-  return items;
+    function createItems() {
+      const maxFive = randomInt(1, 5);
+      const numItems = maxFive();
+      const items = [];
+
+      for (let i = 0; i < numItems; i++) {
+        const quantity = maxFive();
+        const unitPrice = randomInt(549, 99999)();
+        const item = {
+          name: commerce.productName(),
+          description: commerce.productDescription(),
+          quantity,
+          price: quantity * unitPrice,
+        };
+
+        items.push(item);
+      }
+
+      return items;
+    }
+  }
 }
