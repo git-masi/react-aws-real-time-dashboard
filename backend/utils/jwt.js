33e8a56b4f2223759ede712f6183a06e630a8fef
Eric Masi <eric.masi90@gmail.com>
2021-11-03 10:33:38 -0400

    Fix bug

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,16 +1,16 @@
+import { createSecretKey } from 'crypto';
 import { SignJWT, jwtVerify } from 'jose';
 
-// This is only for testing, in production we'd want to use an SSM parameter or Secrets Manager secret
-const clientKey = 'testing123';
+// This is for testing purposes only
+// Use SSM secret parameter or Secrets Manager secret in production
+const APP_SECRET = '0302053c-d0e6-499b-9efe-dad9b629e0ac';
 
-export async function signClientJwt(client) {
-  return await new SignJWT(client)
-    .setProtectedHeader({ alg: 'ES256' })
+export const signClientJwt = async (client) =>
+  await new SignJWT(client)
+    .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
     .setIssuedAt()
-    .setExpirationTime('12h')
-    .sign(clientKey);
-}
+    .setExpirationTime('1h')
+    .sign(createSecretKey(Buffer.from(APP_SECRET, 'utf8')));
 
-export async function verifyClientJwt(jwt) {
-  return await jwtVerify(jwt, clientKey); // { payload, protectedHeader }
-}
+export const verifyClientJwt = async (jwt) =>
+  await jwtVerify(jwt, createSecretKey(Buffer.from(APP_SECRET, 'utf8'))); // { payload, protectedHeader }
